64a68b3397e6eea0fae69886cd695e3123440b63
albaysystem <muratalbay510@gmail.com>
2025-11-01 05:56:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,23 @@
-// ALBAY STEPS Service Worker (basit cache-first)
-const CACHE = "albay-steps-v1";
+// ALBAY STEPS – basit cache-first
+const CACHE = 'albay-steps-v1';
 const ASSETS = [
-  "/albay-panel/",
-  "/albay-panel/index.html",
-  "/albay-panel/panel.html",
-  "/albay-panel/manifest.webmanifest"
+  '/index.html',
+  '/panel.html',
+  '/manifest.json'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', (e)=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', (e)=>{
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then(res=>res||fetch(e.request))
   );
 });
